17bd5ef67e11a861c7b3d473a5b0402daac4e5aa
Mohd Azeem <azm5202@gmail.com>
2022-09-05 20:02:38 +0530

    Added CodeChef Solutions Folder

---
+++ CodeChef Solutions/atm_codechef.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    for (int j = 0; j < t; j++)
+    {
+        int total, n, withdraw;
+        scanf(" %d", &n);
+        char output[n];
+        scanf(" %d", &total);
+
+        for (int i = 0; i < n; i++)
+        {
+            scanf(" %d", &withdraw);
+
+            if (withdraw <= total)
+            {
+                output[i] = '1';
+                total = total - withdraw;
+            }
+            else
+            {
+                output[i] = '0';
+            }
+        }
+        output[n]='\0';
+
+        printf("%s\n", output);
+    }
+
+    return 0;
+}
