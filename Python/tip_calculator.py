6fa8754f3a561ff3e8cc8c926d38d4e771d87b56
Mohd Azeem <azeemidrisi@protonmail.com>
2022-12-03 16:13:59 +0530

    Added line-break

--- Python/tip_calculator.py
+++ Python/tip_calculator.py
@@ -1,7 +1,7 @@
-print("Welcome to the tip calculator.")
-bill = float(input("What was the total bill? : $"))
-tip = int(input("Enter Percentage of Tip you want to give : "))
-ppl = int(input("How many people to split the bill? : "))
+print("\nWelcome to the tip calculator.")
+bill = float(input("\nWhat was the total bill? : $"))
+tip = int(input("\nEnter Percentage of Tip you want to give : "))
+ppl = int(input("\nHow many people to split the bill? : "))
 
 amount = bill + ((tip/100) * bill)
 amount = amount/ppl
@@ -10,4 +10,4 @@ amount = round(amount, 2)
 # The code above this line is correct but if we give input 150,12,5 then output is $33.6
 #  but we want $33.60 so we use formatting
 amount = "{:.2f}".format(amount)
-print(f"Each person should pay : ${amount}")
+print(f"\nEach person should pay : ${amount}")
