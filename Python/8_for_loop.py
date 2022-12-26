3cdf8374425db619b4edbedb8c13ba00b2eef832
Mohd Azeem <azeemidrisi@protonmail.com>
2022-12-26 23:37:28 +0530

    For Loop and Password Generator

---
+++ Python/8_for_loop.py
@@ -0,0 +1,19 @@
+# With list
+sample_list = ["hello", "world", "python", "ubuntu"]
+for x in sample_list:
+    print(x)
+
+# With range
+for number in range(1, 20):
+    print(number)
+
+print("\nWith steps of 5\n")
+# Range with custom steps
+for number in range(1, 20, 5):
+    print(number)
+
+# sum of even between 1 t0 100 including 1 and 100
+sum = 0
+for i in range(0, 101, 2):
+    sum += i
+print(f"\n\nSum : {sum}\n")
